901a9964f33626fbe2ee9ffc83b1cc32beee01ca
Gregg Hogan <21093023+greggoryhogan@users.noreply.github.com>
2025-06-27 10:53:41 -0400

    Add tips

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ while ( have_posts() ){
     echo "2024 brought in 500+ reports; this tiny project's most successful year.<br>";
     echo 'If you have suggestions for improving the report for 2025 and beyond, don&rsquo;t hesitate to <a href="mailto:hello@mynameisgregg.com">reach out</a>!</p>';
 
+    echo '<p>If you&rsquo;re visiting the beach during greenhead season, try these tips to lessen your exposure:</p>';
+    echo '<ul>';
+    echo '<li style="list-style: disc inside;margin-bottom: 3px;">Greenheads are attracted to the ocean&rsquo;s salt, so dry off after swimming</li>';
+    echo '<li style="list-style: disc inside;margin-bottom: 3px;">Visit the beach on breezy days — wind makes it harder for them to fly</li>';
+    echo '<li style="list-style: disc inside;margin-bottom: 3px;">Choose light-colored clothing or wear long sleeves and pants when you can</li>';
+    echo '</ul>';
+    
+
     echo '<div class="gh-spacer"></div>';
     echo '<h2 class="line"><span>Embed <div class="green">&nbsp;The Greenhead Report on your website!</div></span></h2>';
     echo '<p>Select your location(s) and display settings to generate an embeddable code for your website.</p>';
